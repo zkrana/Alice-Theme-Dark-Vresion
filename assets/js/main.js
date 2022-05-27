2d1754769ed3455948789baffc9aed5edee7fb89
Zk Rana <zkranao@gmail.com>
2022-05-27 19:25:10 +0600

    Last 2  section with footer. Tab fix, Backgorund animation add, icon gix etc.

--- assets/js/main.js
+++ assets/js/main.js
@@ -144,20 +144,14 @@ $('.filter-button-group').on( 'click', 'li', function() {
 
  $(document).ready( function() {   
 
-// filter items on button click
-$('.isotope-pager').on( 'click', 'a', function() {
-  var filterValue = $(this).attr('data-page');
+	// filter items on button click
+	$('.isotope-pager').on( 'click', 'a', function() {
+	var filterValue = $(this).attr('data-page');
 
-  $('.isotope-pager a').removeClass('active');
-  $(this).addClass('active');
-});
-    })
-	
-	
-	
-	
-	
-	
+	$('.isotope-pager a').removeClass('active');
+	$(this).addClass('active');
+	});
+})
 	
 
 $(document).ready(function(){
@@ -184,3 +178,170 @@ $('.popupimg').magnificPopup({
 
 });
 
+$(document).ready(function(){
+	$('.contact').mkinfinite({
+	  maxZoom:       1.4,
+	  animationTime: 4000,
+	  imagesRatio:   (960 / 720),
+	  isFixedBG:     true,
+	  zoomIn:        true,
+	  imagesList:    new Array(
+		'assets/images/art-brush-colors-3112.jpg',
+		'assets/images/picjumbo.com_IMG_4563.jpg',
+		'assets/images/picjumbo.com_HNCK1805.jpg'
+	  )
+	});
+  });
+
+  $(document).ready(function(){
+	$('#one').mkinfinite({
+	  maxZoom:       1.4,
+	  animationTime: 4000,
+	  imagesRatio:   (960 / 720),
+	  isFixedBG:     true,
+	  zoomIn:        true,
+	  imagesList:    new Array(
+		'assets/images/Alice-bg-1.jpg',
+		'assets/images/Alice-bg-1.jpg'
+	  )
+	});
+  });
+
+  $(document).ready(function(){
+	$('.about-bg').mkinfinite({
+	  maxZoom:       1.4,
+	  animationTime: 4000,
+	  imagesRatio:   (960 / 720),
+	  isFixedBG:     true,
+	  zoomIn:        true,
+	  imagesList:    new Array(
+		'assets/images/autumn-avenue-path-946.jpg',
+		'assets/images/autumn-avenue-path-946.jpg'
+	  )
+	});
+  });
+
+  $(document).ready(function(){
+	$('#how-we-work').mkinfinite({
+	  maxZoom:       1.4,
+	  animationTime: 4000,
+	  imagesRatio:   (960 / 720),
+	  isFixedBG:     true,
+	  zoomIn:        true,
+	  imagesList:    new Array(
+		'assets/images/picjumbo.com_IMG_7432.jpg',
+		'assets/images/coffee-coffee-machine-cup-3042.jpg'
+	  )
+	});
+  });
+
+  $(document).ready(function(){
+	$('#numbers').mkinfinite({
+	  maxZoom:       1.4,
+	  animationTime: 4000,
+	  imagesRatio:   (960 / 720),
+	  isFixedBG:     true,
+	  zoomIn:        true,
+	  imagesList:    new Array(
+		'assets/images/picjumbo.com_IMG_7432.jpg',
+		'assets/images/coffee-coffee-machine-cup-3042.jpg'
+	  )
+	});
+  });
+
+  $(document).ready(function(){
+	$('.team').mkinfinite({
+	  maxZoom:       1.4,
+	  animationTime: 4000,
+	  imagesRatio:   (960 / 720),
+	  isFixedBG:     true,
+	  zoomIn:        true,
+	  imagesList:    new Array(
+		'assets/images/3F7D411CC8.jpg',
+		'assets/images/3F7D411CC8.jpg'
+	  )
+	});
+  });
+
+  $(document).ready(function(){
+	$('.service').mkinfinite({
+	  maxZoom:       1.4,
+	  animationTime: 4000,
+	  imagesRatio:   (960 / 720),
+	  isFixedBG:     true,
+	  zoomIn:        true,
+	  imagesList:    new Array(
+		'assets/images/coffee-coffee-machine-cup-3042.jpg',
+		'assets/images/coffee-coffee-machine-cup-3042.jpg'
+	  )
+	});
+  });
+
+  $(document).ready(function(){
+	$('.slogan').mkinfinite({
+	  maxZoom:       1.4,
+	  animationTime: 4000,
+	  imagesRatio:   (960 / 720),
+	  isFixedBG:     true,
+	  zoomIn:        true,
+	  imagesList:    new Array(
+		'assets/images/art-brush-colors-3112.jpg',
+		'assets/images/picjumbo.com_IMG_4563.jpg',
+		'assets/images/picjumbo.com_HNCK1805.jpg'
+	  )
+	});
+  });
+
+  $(document).ready(function(){
+	$('.works').mkinfinite({
+	  maxZoom:       1.4,
+	  animationTime: 4000,
+	  imagesRatio:   (960 / 720),
+	  isFixedBG:     true,
+	  zoomIn:        true,
+	  imagesList:    new Array(
+		'assets/images/219FB68281.jpg',
+		'assets/images/219FB68281.jpg'
+	  )
+	});
+  });
+
+  $(document).ready(function(){
+	$('.skill').mkinfinite({
+	  maxZoom:       1.4,
+	  animationTime: 4000,
+	  imagesRatio:   (960 / 720),
+	  isFixedBG:     true,
+	  zoomIn:        true,
+	  imagesList:    new Array(
+		'assets/images/219FB68281.jpg',
+		'assets/images/219FB68281.jpg'
+	  )
+	});
+  });
+
+
+  $(document).ready(function(){
+	$('.process-bg').mkinfinite({
+	  maxZoom:       1.4,
+	  animationTime: 4000,
+	  imagesRatio:   (960 / 720),
+	  isFixedBG:     true,
+	  zoomIn:        true,
+	  imagesList:    new Array(
+		'assets/images/art-brush-colors-3112.jpg',
+		'assets/images/art-brush-colors-3112.jpg'
+	  )
+	});
+  });
+
+  $( document ).ready(function() {
+	//Calls the modal when button is clicked
+	$('#jsModal').on('click', function(){
+	  $('#myModal').modal()
+	});
+	// Calls when modal hides
+	$('#myModal').on('hidden.bs.modal', function (e) {
+	  alert('The modal closed')
+	})
+  });
